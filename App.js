a3af96c7c2034990cfc59ca5e9c0640cd4679a33
johnnyli <johnnyli@stanford.edu>
2017-10-20 06:15:26 +0100

    First Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hi Jonathan!</Text>
+        <Text>Lets Start a new Project!</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
